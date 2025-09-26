9946a735785826b4c7a7ae2ad58ea307d192fa52
Tuo Nome <tuoindirizzo@email.com>
2025-09-26 10:43:12 +0100

    Test cambio titolo

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="bg-blue-700 text-white px-8 py-5 flex items-center justify-between shadow-lg">
-      <div className="font-bold text-2xl tracking-wide">Emanueleee Rotundi Projects</div>
+      <div className="font-bold text-2xl tracking-wide">Emanuele Rotundi Projects</div>
       <ul className="flex gap-10">
         <li><Link to="/" className="hover:text-yellow-300 transition px-3 py-2 rounded-md">Home</Link></li>
         <li><Link to="/goals" className="hover:text-yellow-300 transition px-3 py-2 rounded-md">Goals</Link></li>
